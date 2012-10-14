b50d254d25d833a6697a7722b0b22334019b026c
Thomas Levine <occurrence@thomaslevine.com>
2012-10-14 14:22:46 -0400

    history

--- modules/history/init.zsh
+++ modules/history/init.zsh
@@ -10,7 +10,7 @@
 # Variables
 #
 
-HISTFILE="${ZDOTDIR:-$HOME}/.zhistory"       # The path to the history file.
+HISTFILE="${ZDOTDIR:-$HOME}/.zhistory/current"       # The path to the history file.
 HISTSIZE=10000                   # The maximum number of events to save in the internal history.
 SAVEHIST=10000                   # The maximum number of events to save in the history file.
 
@@ -23,11 +23,11 @@ setopt EXTENDED_HISTORY          # Write the history file in the ':start:elapsed
 setopt INC_APPEND_HISTORY        # Write to the history file immediately, not when the shell exits.
 setopt SHARE_HISTORY             # Share history between all sessions.
 setopt HIST_EXPIRE_DUPS_FIRST    # Expire a duplicate event first when trimming history.
-setopt HIST_IGNORE_DUPS          # Do not record an event that was just recorded again.
-setopt HIST_IGNORE_ALL_DUPS      # Delete an old recorded event if a new event is a duplicate.
+unsetopt HIST_IGNORE_DUPS          # Do not record an event that was just recorded again.
+unsetopt HIST_IGNORE_ALL_DUPS      # Delete an old recorded event if a new event is a duplicate.
 setopt HIST_FIND_NO_DUPS         # Do not display a previously found event.
 setopt HIST_IGNORE_SPACE         # Do not record an event starting with a space.
-setopt HIST_SAVE_NO_DUPS         # Do not write a duplicate event to the history file.
+unsetopt HIST_SAVE_NO_DUPS       # Write duplicate events to the history file.
 setopt HIST_VERIFY               # Do not execute immediately upon history expansion.
 setopt HIST_BEEP                 # Beep when accessing non-existent history.
 
@@ -37,3 +37,24 @@ setopt HIST_BEEP                 # Beep when accessing non-existent history.
 
 # Lists the ten most used commands.
 alias history-stat="history 0 | awk '{print \$2}' | sort | uniq -c | sort -n -r | head"
+
+# Archive the history
+_archive_history() {
+  # Do nothing if the file is small.
+  test `wc -l "${ZDOTDIR:-$HOME}/.zhistory/current"|cut -d\  -f1` -lt 1000 && return
+
+  # Timestamp
+  now="`date --rfc-3339 seconds`"
+
+  # Move the file
+  mv "${ZDOTDIR:-$HOME}/.zhistory/current" "${ZDOTDIR:-$HOME}/.zhistory/$now"
+
+  # Add and commit it.
+  (
+    cd "${ZDOTDIR:-$HOME}/.zhistory"
+    git init
+    git add .
+    git commit . -m "$now"
+  )
+}
+add-zsh-hook precmd _archive_history
