3f9eab1a201d8328d555c6e96770643e728a7aa3
Thomas Levine <occurrence@thomaslevine.com>
2012-10-14 14:25:20 -0400

    comments

--- modules/history/init.zsh
+++ modules/history/init.zsh
@@ -23,8 +23,8 @@ setopt EXTENDED_HISTORY          # Write the history file in the ':start:elapsed
 setopt INC_APPEND_HISTORY        # Write to the history file immediately, not when the shell exits.
 setopt SHARE_HISTORY             # Share history between all sessions.
 setopt HIST_EXPIRE_DUPS_FIRST    # Expire a duplicate event first when trimming history.
-unsetopt HIST_IGNORE_DUPS          # Do not record an event that was just recorded again.
-unsetopt HIST_IGNORE_ALL_DUPS      # Delete an old recorded event if a new event is a duplicate.
+unsetopt HIST_IGNORE_DUPS        # Record an event that was just recorded again.
+unsetopt HIST_IGNORE_ALL_DUPS    # Keep an old recorded event if a new event is a duplicate.
 setopt HIST_FIND_NO_DUPS         # Do not display a previously found event.
 setopt HIST_IGNORE_SPACE         # Do not record an event starting with a space.
 unsetopt HIST_SAVE_NO_DUPS       # Write duplicate events to the history file.
